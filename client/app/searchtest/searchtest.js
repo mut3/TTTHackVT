85a854d85cbc2810a745cca9b1eb62d6132a68ed
Dillon Beliveau <dillonbeliveau@gmail.com>
2014-10-11 02:56:35 -0400

    Map working on searchtest

--- client/app/searchtest/searchtest.js
+++ client/app/searchtest/searchtest.js
@@ -76,6 +76,6 @@ jQuery(document).ready(function($) {
     });
 
     $(document).on("submit", "#destinationSearchForm", function() {
-        $("#locationIcon").click();
+        $("#nearestButton").click();
     });
 });
