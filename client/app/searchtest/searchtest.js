2cfce5d9ed611a952578ec74b0f9fcbc96d68d1e
Dillon Beliveau <dillonbeliveau@gmail.com>
2014-10-11 10:38:30 -0400

    Added click handlers on markers

--- client/app/searchtest/searchtest.js
+++ client/app/searchtest/searchtest.js
@@ -16,11 +16,16 @@ jQuery(document).ready(function($) {
 
     function mapAddPin(name, latitude, longitude) {
         console.log([name,latitude,longitude,map]);
-        markers.push(new google.maps.Marker({
+        var mark = new google.maps.Marker({
             position: { lat: latitude, lng: longitude},
             map: window.map,
             title: name
-        }));
+        });
+        google.maps.event.addListener(mark, "click", function() {
+            //console.log(mark.getPosition().lat(), mark.getPosition().lng());
+            getattractions(mark.getPosition().lat(), mark.getPosition().lng());
+        });
+        markers.push(mark);
     }
 
     function mapClear(map) {
